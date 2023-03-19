2c981b36c56ce379b8dc0a3857838c363da64a22
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-03-18 23:01:00 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-connect = document.getElementById('connect')
-connect_menu = document.getElementById('connect-dropdown')
-connect_svg = document.getElementById('connect-svg')
+connect = document.querySelector('#connect')
+connect_menu = document.querySelector('#connect-dropdown')
+connect_svg = document.querySelector('#connect-svg')
+burger = document.querySelectorAll('.burger-menu img')
+burger_open = document.querySelector('.burger-menu img:last-child')
+burger_close = document.querySelector('.burger-menu img:first-child')
+burger_menu = document.querySelector('.menu')
+burger_connect = document.querySelector('#burger-connect')
+burger_connect_menu = document.querySelector('#burger-connect-menu')
+burger_connect_svg = document.querySelector('.burger-connect svg')
+
+
+
 
 document.addEventListener('mouseup', e => {
     
@@ -24,3 +34,28 @@ connect.addEventListener("click", e => {
     }
 
 })
+
+burger[0].addEventListener("click", () => {
+    burger[0].style.display = 'none'
+    burger[1].style.display = 'block'
+    burger_menu.style.display = 'flex'
+
+})
+
+burger[1].addEventListener("click", () => {
+    burger[1].style.display = 'none'
+    burger[0].style.display = 'block'
+    burger_menu.style.display = 'none'
+
+})
+
+burger_connect.addEventListener("click", e => {
+
+    if (burger_connect_menu.style.display == 'inline') {
+        burger_connect_menu.style.display = 'none'
+        burger_connect_svg.style.transform = 'scaleY(1)'
+    
+    } else {
+        burger_connect_menu.style.display = 'inline'
+        burger_connect_svg.style.transform = 'scaleY(-1)'
+    }})
